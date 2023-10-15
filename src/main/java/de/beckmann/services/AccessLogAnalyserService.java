7e597859530426c90188e6c074d1c3862b0457ad
Nico Beckmann <nico.beckmann>
2023-10-15 22:13:36 +0200

    refactor error handling

--- src/main/java/de/beckmann/services/AccessLogAnalyserService.java
+++ src/main/java/de/beckmann/services/AccessLogAnalyserService.java
@@ -18,6 +18,8 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class AccessLogAnalyserService {
 
     private static final String TIMESTAMP_FORMAT = "dd/MMM/yyyy:HH:mm:ss";
@@ -25,52 +27,55 @@ public class AccessLogAnalyserService {
     private static final String CSV_FORMAT = "%s;%d\n";
     private static final String REGEX_TIMESTAMP = "\\d{2}\\/\\w{3}\\/\\d{4}:\\d{2}:\\d{2}:\\d{2}";
 
-    public void analyseRequestsPerSecond(String inputFilePath, String outputFilePath) throws IOException
-    {
+    public void analyseRequestsPerSecond(String inputFilePath, String outputFilePath) throws IOException {
+        verifyInputs(inputFilePath, outputFilePath);
         final List<LocalDateTime> timestamps = readTimestamps(inputFilePath);
 
-            final Map<LocalDateTime, Long> counts = countEntriesPerSecond(timestamps);
+        final Map<LocalDateTime, Long> counts = countEntriesPerSecond(timestamps);
 
-            writeToCsv(outputFilePath, counts);
-            for (final Map.Entry<LocalDateTime, Long> entry : counts.entrySet()) {
-                System.out.println("Timestamp " + entry.getKey() + " had " + entry.getValue() + " entries.");
-            }
+        writeToCsv(outputFilePath, counts);
+        for (final Map.Entry<LocalDateTime, Long> entry : counts.entrySet()) {
+            System.out.println("Timestamp " + entry.getKey() + " had " + entry.getValue() + " entries.");
+        }
     }
 
-    private static void writeToCsv(final String outputFile, final Map<LocalDateTime, Long> counts) throws IOException
-    {
+    private static void writeToCsv(final String outputFile, final Map<LocalDateTime, Long> counts) throws IOException {
         final Path path = Path.of(outputFile);
-        
+
         Files.write(path, HEADER.getBytes());
-        for(final Entry<LocalDateTime, Long> entry : counts.entrySet())
-        {
-            final String entryString =String.format(CSV_FORMAT, entry.getKey(), entry.getValue());
-            Files.write(path, entryString.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND);
+        for (final Entry<LocalDateTime, Long> entry : counts.entrySet()) {
+            final String entryString = String.format(CSV_FORMAT, entry.getKey(), entry.getValue());
+            Files.write(path, entryString.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.WRITE,
+                    StandardOpenOption.APPEND);
         }
     }
 
-    
-    private static List<LocalDateTime> readTimestamps(final String filePath) {
-
-        try {
-            final Stream<String> lines = Files.lines(Paths.get(filePath));
-            final List<LocalDateTime> timestamps = new ArrayList<>();
-            
-            lines.forEach(line -> {
-                final LocalDateTime dt = getTimstampFromEntry(line);
-                if (dt != null) {
-                    timestamps.add(dt);
-                }
-
-            });
-
-            lines.close();
-            return timestamps;
-        } catch (final IOException e) {
-            e.printStackTrace();
+    private static void verifyInputs(String inputFilePath, String outputFilePath) throws IllegalArgumentException
+    {
+        if(StringUtils.isBlank(inputFilePath))
+        {
+            throw new IllegalArgumentException("inputFilePath must not be empty!");
         }
+        if(StringUtils.isBlank(outputFilePath))
+        {
+            throw new IllegalArgumentException("outputFilePath must not be empty!");
+        }
+    }
 
-        return new ArrayList<LocalDateTime>();
+    private static List<LocalDateTime> readTimestamps(final String filePath) throws IOException {
+        final Stream<String> lines = Files.lines(Paths.get(filePath));
+        final List<LocalDateTime> timestamps = new ArrayList<>();
+
+        lines.forEach(line -> {
+            final LocalDateTime dt = getTimstampFromEntry(line);
+            if (dt != null) {
+                timestamps.add(dt);
+            }
+
+        });
+
+        lines.close();
+        return timestamps;
     }
 
     private static LocalDateTime getTimstampFromEntry(final String entry) {
@@ -92,11 +97,11 @@ public class AccessLogAnalyserService {
 
     private static LocalDateTime convertStringToTimestamp(final String timestampString) {
         try {
-            
+
             if (timestampString == null) {
                 return null;
             }
-            
+
             final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(TIMESTAMP_FORMAT, Locale.ENGLISH);
             return LocalDateTime.from(formatter.parse(timestampString));
         } catch (final DateTimeParseException e) {
